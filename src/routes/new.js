8031122bd32a349e8a6d4b8a32eecff3d6a110e9
Alexandre Sauner <alexandre@trackit.io>
2020-07-23 18:16:42 +0200

    Added dest URL checking to prevent redirect loops and invalid URLs

--- src/routes/new.js
+++ src/routes/new.js
@@ -1,3 +1,4 @@
+const frontendHostname = process.env.FRONTEND_HOSTNAME || null;
 let db;
 
 module.exports = (dbLink) => {
@@ -6,13 +7,24 @@ module.exports = (dbLink) => {
     return (req, res) => {
         const { dest } = req.body;
 
-        if (typeof(dest) != "string" || dest.length <= 0) {
+        if (typeof(dest) != "string") {
+            res.sendStatus(400);
+            return;
+        }
+
+        try {
+            const destUrl = new URL(dest);
+
+            if (frontendHostname != null && destUrl.hostname == frontendHostname) {
+                res.sendStatus(400);
+                return;
+            }
+        } catch (e) {
             res.sendStatus(400);
             return;
         }
 
         const short = dbLink.insert(dest);
-
         res.json({
             'short': short
         });
